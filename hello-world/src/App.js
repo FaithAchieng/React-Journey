3937d9f881e5a504676e804a7d2f0f1261ffca06
FaithAchieng <faithachienglexus@gmail.com>
2024-12-11 18:13:49 +0300

    first commit

---
+++ hello-world/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet'
+import Welcome from'./components/welcome'
+import Hello from'./components/Hello'
+import Message from'./components/Message'
+import Counter from'./components/Counter'
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
+function App() {
+  return (
+    <div className="App">
+      <EventBind />
+     {/*} <ClassClick />
+      <FunctionClick />*/}
+      {/*<Counter />*/}
+     {/* <Message /> */}
+     {/*} <Greet name='Bruce' heroName='Batman' >
+       <p>This is children props</p>
+      </Greet>
+      <Greet name='Clark' heroName='Superman'>
+        <button>Action</button>
+      </Greet>
+      <Greet name='Diana' heroName='Wonder woman' />
+       <Welcome name='Bruce' heroName='Batman' /> 
+       <Welcome name='Clark' heroName='Superman'/> 
+       <Welcome name='Diana' heroName='Wonder woman'/> 
+      {/*<Hello />*/}
+    </div>
+  );
+}
+
+export default App;
