56c8e9e8f5f53c4eb6d380ce3d987cc46bd87962
FaithAchieng <faithachienglexus@gmail.com>
2025-02-24 10:19:59 +0300

    Finished card component

--- cards/src/Card.jsx
+++ cards/src/Card.jsx
@@ -2,9 +2,11 @@ import profilePic from './assets/profile.jpg';
 function Card(){
     return(
         <div className="card">
-            <img src={profilePic} alt="profile picture"></img>
-            <h2>Faith Achieng</h2>
-            <p>Computer Science Student</p>
+            <img className="card-image" src={profilePic} alt="profile picture"></img>
+            <h2 className="card-title">Faith Achieng</h2>
+            <p className="card-text">Computer Science Student, Currently doing my attachment.
+
+            </p>
 
         </div>
     );
