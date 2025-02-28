837b33c4002ede94cd9dee79fe188bfd6c560745
FaithAchieng <faithachienglexus@gmail.com>
2025-02-28 15:32:05 +0300

    Added prop types

--- greeting/src/UserGreeting.jsx
+++ greeting/src/UserGreeting.jsx
@@ -5,7 +5,19 @@ function UserGreeting(props){
     // }else{
     //   return <h2>Please log in to continue</h2>  
     // }
-    return(props.isLoggedIn ? <h2 className="welcome-message">Welcome{props.username}</h2> : 
-    <h2 className="login-prompt">Please log in to continue</h2>);
+    const welcomeMessage=<h2 className="welcome-message">Welcome{props.username}</h2>
+    const loginPrompt=<h2 className="login-prompt">Please log in to continue</h2>
+//     return(props.isLoggedIn ? <h2 className="welcome-message">Welcome{props.username}</h2> : 
+//     <h2 className="login-prompt">Please log in to continue</h2>);
+return(props.isLoggedIn ? welcomeMessage : loginPrompt);
+}
+UserGreeting.proptypes={
+    isLoggedIn:PropTypes.bool,
+    username:PropTypes.string
+ 
+}
+UserGreeting.defaultProps={
+    isLoggedIn: false,
+    username: "Guest"
 }
 export default UserGreeting
